3eeb7cb1e5f9fa6d824f965e81fda71b94353c43
Kadeev Daniil <daniil.kadeev@yandex.ru>
2024-11-09 12:54:24 +0300

    Simple

--- var_arithmetic/2_5_scanf.c
+++ var_arithmetic/2_5_scanf.c
@@ -17,8 +17,7 @@ int main(void)
 
     char byte1 = '0', byte2 = '0';
 
-    int res1 = scanf("%c", &byte1);
-    int res2 = scanf("%c", &byte2);
+    int res1 = scanf("%c", &byte1, &byte2);
 
     printf("byte1 = %c, byte2 = %c", byte1, byte2);
     return 0;
